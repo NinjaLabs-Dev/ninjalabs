283fb006f6b29a00f96b7762ce72fe80fe1271ff
NinjaLabs-Dev <fraser@ninjalabs.dev>
2022-02-05 00:37:33 +0000

    Fonts

--- src/App.vue
+++ src/App.vue
@@ -69,7 +69,6 @@ export default  {
 		link: [
 			{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
 			{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: true },
-			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;700&display=swap'}
 		]
 	},
 	data() {
