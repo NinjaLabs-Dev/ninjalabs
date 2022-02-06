39d562f077f2174f06b0a59ae106eeafded0b7b4
NinjaLabs-Dev <fraser@ninjalabs.dev>
2022-02-06 01:47:19 +0000

    SEO & Sizing optimization

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,11 @@ module.exports = {
 	configureWebpack:  {
 		plugins: process.env.NODE_ENV === 'production' ? [
 			new PrerenderSPAPlugin({
-				// Required - The path to the webpack-outputted app to prerender.
 				staticDir: path.join(__dirname, 'dist'),
-				headless: true,
-				// Required - Routes to render.
 				routes: [ '/', '/contact', '/me', '/projects'],
+				renderer: new Renderer({
+					headless: true,
+				})
 			})
 		] : []
 	}
