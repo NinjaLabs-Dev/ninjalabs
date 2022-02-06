9ef1760e8696e5e0286f7da393dd8c08d09fb1d8
NinjaLabs-Dev <fraser@ninjalabs.dev>
2022-02-06 01:48:35 +0000

    SEO & Sizing optimization

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 module.exports = {
 	configureWebpack:  {
